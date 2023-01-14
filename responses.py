4f2346a3d5b41faa9361e98a72b178e0a0b1fe46
aarcee17 <106922705+aarcee17@users.noreply.github.com>
2023-01-15 03:40:21 +0530

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,5 +1,11 @@
 import random
 
+from datetime import datetime
+import time 
+now = datetime.now()
+hrs = now.hour
+print('Datetime is:', now)
+day = now.strftime('%A'))
 
 def get_response(message: str) -> str:
     p_message = message.lower()
@@ -14,3 +20,6 @@ def get_response(message: str) -> str:
         return '`This is a help message that you can modify.`'
 
     return 'I didn\'t understand what you wrote. Try typing "!help".'
+def get_intresponse(message:int) -> int:
+    
+    
